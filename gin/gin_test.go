f4e24b4a705b999c68a50a9a8b4be21ed4de47bb
Allan Tabilog <allan.tabilog@pexa.com.au>
2024-12-12 17:24:27 +1100

    Gin getting started code and test

---
+++ gin/gin_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPingRoute(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "pong", w.Body.String())
+}
+
+func TestPostUser(t *testing.T) {
+	router := setupRouter()
+	router = postUser(router)
+
+	w := httptest.NewRecorder()
+
+	exampleUser := User{
+		Username: "test",
+		Gender:   "male",
+	}
+
+	userJson, _ := json.Marshal(exampleUser)
+	req, _ := http.NewRequest("POST", "/user/add", strings.NewReader(string(userJson)))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, string(userJson), w.Body.String())
+}
